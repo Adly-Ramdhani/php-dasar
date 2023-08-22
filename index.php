94c8d49f143ac51f11a62b7fb25e3f70a860a1b1
Adly Ramdhani <AdlyRamdhani@MacBook-Pro.local>
2023-08-22 10:56:06 +0700

    phpDasar

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$angka = 1;
+
+while ($angka <= 50) {
+    if ($angka % 2 == 1) {
+        echo $angka . " adalah bilangan Genap" ;
+    
+    } else {
+        echo $angka . " adalah bilangan Ganjil";
+    }
+    $angka++;
+}
+?>
+
+
+
